d06b2176ea49ec7ea5a9a9eaa2112174a5d2c0dc
Yash Kathe <yashkathe123@gmail.com>
2022-01-13 21:57:14 +0530

    exit codes added + correct path added

--- addtheme.sh
+++ addtheme.sh
@@ -17,9 +17,9 @@ initFunction() {
 	if [[ ${UID} -eq 0 ]]; then
 		echo "Starting ... "
 		sleep 1s
+		echo ""
 		echo "Make sure you are running the script from where the folders are located"
-		echo "Icons and Cursors are located in /usr/share/icons"
-		echo "Themes are located in /usr/share/themes"
+		echo ""
 		sleep 1s
 	else
 		echo "ERROR ! start this script as root"
@@ -43,14 +43,11 @@ readFoldersFunction() {
 		test -n "$FOLDER_NAME" && break
 		echo ">>> Invalid Selection"
 	done
-
-	echo "you selected $FOLDER_NAME"
-
 }
 
 # printing the full path
 
-getFolderPath() {
+getFolderPathFunction() {
 
 	# PATH_NAME=$(cd "$FOLDER_NAME" && pwd)
 
@@ -61,16 +58,29 @@ getFolderPath() {
 	FOLDER_SUFFIX=${filename##*.}
 }
 
-extractFolder() {
+extractFolderFunction() {
 	#checking if its a tar file or normal so to extract
 
 	if [[ "${FOLDER_SUFFIX}" -eq 'xz' ]]; then
 		echo ""
 		echo "unziping the file ... $FOLDER_NAME "
+		sleep 1.2
 		tar -xf ${FOLDER_NAME}
-		sleep 1s
-		FOLDER_PREFIX=${filename%%.*}
-		echo "folder just unzipped was $FOLDER_PREFIX"
+
+		#exit if there is a error
+
+		if [[ ${?} -ne 0 ]]; then
+			echo ""
+			echo "report errors at https://github.com/yashkathe/script-for-adding-themes-and-icons"
+			echo ""
+			exit 1
+		else
+			FOLDER_PREFIX=${filename%%.*}
+			echo ""
+			echo "folder just unzipped was $FOLDER_PREFIX"
+			echo ""
+		fi
+
 	else
 		echo "not xz"
 	fi
@@ -78,15 +88,44 @@ extractFolder() {
 
 pasteFoldersFunction() {
 	echo ""
-	read -p "Enter: 1 if this is a theme | Enter: 2 for icons and cursor: " PASTE_NUM
+	read -p "Enter: 1 if this is a theme | Enter: 2 for icons/cursor: " PASTE_NUM
 
 	if [[ ${PASTE_NUM} -eq 1 ]]; then
-		mv ${FOLDER_PREFIX} /home/themes
+		mv ${FOLDER_PREFIX} /usr/share/themes
+		if [[ ${?} -ne 0 ]]; then
+			echo ""
+			echo "report errors at https://github.com/yashkathe/script-for-adding-themes-and-icons"
+			echo "exiting now"
+			echo ""
+			exit 1
+		else
+			echo ""
+			echo "file moved successfully"
+			echo ""
+		fi
 	elif [[ ${PASTE_NUM} -eq 2 ]]; then
-		mv ${FOLDER_PREFIX} /home/icons
+		sleep 1.5
+		mv ${FOLDER_PREFIX} /usr/share/icons
+		if [[ ${?} -ne 0 ]]; then
+			echo ""
+			echo "report errors at https://github.com/yashkathe/script-for-adding-themes-and-icons"
+			echo "exiting now"
+			echo ""
+			exit 1
+		else
+			echo ""
+			echo "file moved successfully"
+			echo ""
+		fi
 	else
 		echo "enter correct folder"
 	fi
+}
+
+helpFunction() {
+	echo ""
+	echo "Source Code: https://github.com/yashkathe/script-for-adding-themes-and-icons"
+	echo ""
 }
 
 exitFunction() {
@@ -97,17 +136,18 @@ case "${1}" in
 --start)
 	initFunction
 	readFoldersFunction
-	getFolderPath
-	extractFolder
+	getFolderPathFunction
+	extractFolderFunction
 	pasteFoldersFunction
 	;;
+--help)
+	helpFunction
+	;;
 *)
-	exitFunction
+	initFunction
+	readFoldersFunction
+	getFolderPathFunction
+	extractFolderFunction
+	pasteFoldersFunction
 	;;
 esac
-
-	*)
-	exitFunction
-	;;
-esac
-
